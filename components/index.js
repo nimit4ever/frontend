3263f17b91b11110e3d42c437f49afcda55a8153
Nimit Patel <nimit4ever@gmail.com>
2024-03-24 23:32:04 +0530

    Badge remove

--- components/index.js
+++ components/index.js
@@ -1,8 +1,7 @@
 import MetaData from "./MetaData.jsx";
 import Loader from "./Loader.jsx";
-import OfferBadge from "./OfferBadge.jsx";
 import StarRating from "./StarRating.jsx";
 import AmountShow from "./AmountShow.jsx";
 import { toast as ToastMassage } from "react-toastify";
 
-export { MetaData, Loader, OfferBadge, StarRating, AmountShow, ToastMassage };
+export { MetaData, Loader, StarRating, AmountShow, ToastMassage };
